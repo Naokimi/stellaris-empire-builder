fbd4400a3336c288ecfb1e99cd94fb231be47753
Paulo DAlberti <phdalberti@gmail.com>
2020-01-19 20:39:40 +0900

    small refactoring - need to reduce method line size

--- app/services/scraper_service.rb
+++ app/services/scraper_service.rb
@@ -1,18 +1,24 @@
 require 'open-uri'
 
 class ScraperService
+  def civics_scraper(tables_index, slice_size)
+    tables = tables_scraper('https://stellaris.paradoxwikis.com/Civics')
+    mapped_tables = tables[tables_index].search('td').map do |element|
+      icon_src = element.children.children.attribute('src')
+      icon_src && element.text.strip.empty? ? icon_src.value : element.text.strip
+    end
+    slicer(mapped_tables, slice_size)
+  end
+
   def ethics_scraper
-    url = 'https://stellaris.paradoxwikis.com/Ethics'
-    document = html_doc(url)
-    tables = tables_scraper(url, document)
-    scraped_array = []
+    document = html_doc('https://stellaris.paradoxwikis.com/Ethics')
     last_gestalt_index = 67
     sanitized_range = 0..-7
 
-    mapped_headlines = default_scraper(url, 'h3', document).map do |element|
+    mapped_headlines = default_scraper(document, 'h3').map do |element|
       element.text.strip[sanitized_range]
     end
-    mapped_tables = tables.search('td').each_with_index.map do |element, i|
+    mapped_tables = tables_scraper(document).search('td').each_with_index.map do |element, i|
       icon_src = element.children.children.children.attribute('src')
       if i > last_gestalt_index
         nil
@@ -22,24 +28,12 @@ class ScraperService
         element.text.strip
       end
     end
-    mapped_tables.compact.each_slice(4) { |slice| scraped_array << slice }
-    scraped_array
-  end
-
-  def civics_scraper(tables_index, slice_size)
-    tables = tables_scraper('https://stellaris.paradoxwikis.com/Civics')
-    scraped_array = []
-    tables[tables_index].search('td').map do |element|
-      icon_src = element.children.children.attribute('src')
-      icon_src && element.text.strip.empty? ? icon_src.value : element.text.strip
-    end.each_slice(slice_size) { |slice| scraped_array << slice }
-    scraped_array
+    slicer(mapped_tables.compact, 4)
   end
 
   def traits_scraper(tables_index, slice_size)
     tables = tables_scraper('https://stellaris.paradoxwikis.com/Traits')
-    scraped_array = []
-    tables[tables_index].search('td').each_with_index.map do |element, i|
+    mapped_tables = tables[tables_index].search('td').each_with_index.map do |element, i|
       lithoid_restriction = element.children.attribute('alt')
       icon_src = element.children.children.attribute('src')
       if lithoid_restriction
@@ -51,22 +45,29 @@ class ScraperService
       else
         element.text.strip
       end
-    end.each_slice(slice_size) { |slice| scraped_array << slice }
-    scraped_array
+    end
+    slicer(mapped_tables, slice_size)
   end
 
   private
 
-  def tables_scraper(url, document = html_doc(url))
-    document.search('.mildtable tbody')
-  end
-
-  def default_scraper(url, target, document = html_doc(url))
+  def default_scraper(url_or_doc, target)
+    document = url_or_doc.class == String ? html_doc(url_or_doc) : url_or_doc
     document.search(target)
   end
 
   def html_doc(url)
     html_file = open(url).read
     Nokogiri::HTML(html_file)
+  end
+
+  def slicer(array, slice_size)
+    sliced_array = []
+    array.each_slice(slice_size) { |slice| sliced_array << slice }
+    sliced_array
+  end
+
+  def tables_scraper(url_or_doc)
+    default_scraper(url_or_doc, '.mildtable tbody')
   end
 end
