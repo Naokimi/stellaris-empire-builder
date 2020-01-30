b3c89310f877d1811fb82a0e022d576819618879
Paulo DAlberti <phdalberti@gmail.com>
2020-01-30 20:52:52 +0900

    government validators

--- app/models/government.rb
+++ app/models/government.rb
@@ -1,2 +1,15 @@
 class Government < ApplicationRecord
+  AUTHORITIES = %w[Humanoid
+                   Mammalian
+                   Reptilian
+                   Avian
+                   Arthropoid
+                   Molluscoid
+                   Fungoid
+                   Plantoid
+                   Lithoid
+                   Machine].freeze
+
+  validates :type, inclusion: { in: AUTHORITIES }
+  validates :icon, presence: true
 end
