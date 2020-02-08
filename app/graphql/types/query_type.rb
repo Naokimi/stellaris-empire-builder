fde0d6aadb24683743203ffb4814007903c04dc7
Paulo DAlberti <phdalberti@gmail.com>
2020-02-08 19:30:43 +0900

    untested trait type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,12 +2,17 @@ module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
+    field :traits, [Types::TraitType], null: false
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
+    field :test_field, String, null: false, description: "An example field added by the generator"
+
     def test_field
       "Hello World!"
+    end
+
+    def traits
+      Trait.all
     end
   end
 end
--- app/graphql/types/trait_type.rb
+++ app/graphql/types/trait_type.rb
@@ -1,4 +1,13 @@
 module Types
   class TraitType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :icon, String, null: false
+    field :value, Integer, null: false
+    field :effects, String, null: true
+    field :description, String, null: true
+    field :category, String, null: true
+    field :type, String, null: false
+    # field :species, [Types::SpeciesType], null: true
   end
 end
