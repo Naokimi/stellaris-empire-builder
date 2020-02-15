23df940e8cbe4381a1225c9bc7a823cb7ebd626d
Paulo DAlberti <phdalberti@gmail.com>
2020-02-15 17:58:11 +0900

    graphql civics

--- app/graphql/types/civic_type.rb
+++ app/graphql/types/civic_type.rb
@@ -1,4 +1,10 @@
 module Types
   class CivicType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :icon, String, null: false
+    field :effects, String, null: true
+    field :description, String, null: true
+    field :group, String, null: false
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -23,5 +23,27 @@ module Types
     def traits_by_group(group:)
       Trait.where(group: group)
     end
+
+    field :civics, [Types::CivicType], null: false
+
+    def civics
+      Civic.all
+    end
+
+    field :civic, Types::CivicType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def civic(id:)
+      Civic.find(id)
+    end
+
+    field :civics_by_group, [Types::CivicType], null: false do
+      argument :group, String, required: true
+    end
+
+    def civics_by_group(group:)
+      Civic.where(group: group)
+    end
   end
 end
