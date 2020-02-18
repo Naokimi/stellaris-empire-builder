20256c8d5d5e5565c05955e0579853f26e827bb4
Paulo DAlberti <phdalberti@gmail.com>
2020-02-18 16:03:28 +0900

    added relation arguments to create empire

--- app/graphql/mutations/create_empire.rb
+++ app/graphql/mutations/create_empire.rb
@@ -1,11 +1,15 @@
 class Mutations::CreateEmpire < Mutations::BaseMutation
   argument :name, String, required: true
+  argument :species, Types::SpeciesType, required: true
+  argument :government, Types::GovernmentType, required: true
 
   field :empire, Types::EmpireType, null: false
   field :errors, [String], null: false
 
-  def resolve(name:)
+  def resolve(name:, species:, government:)
     empire = Empire.new(name: name)
+    empire.species = species
+    empire.government = government
     if empire.save
       {
         empire: empire,
